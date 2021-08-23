5687f6ec56f7bd5807cd949b91a226c39b9fa3a7
NickBwalley <nickbwalley@gmail.com>
2021-08-24 00:00:04 +0300

    JavaFX-MiniProject

---
+++ src/lims/admin/AdminController.java
@@ -0,0 +1,30 @@
+package lims.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminController {
+
+    @FXML
+    private Button users;
+
+    @FXML
+    private Button borrowed;
+
+    @FXML
+    void addBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBorrowed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewUsers(ActionEvent event) {
+
+    }
+
+}
---
+++ src/lims/students/StudentController.java
@@ -0,0 +1,25 @@
+package lims.students;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StudentController {
+
+    @FXML
+    private Button borrowbook;
+
+    @FXML
+    private Button returnbook;
+    
+    @FXML
+    void borrowBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnBook(ActionEvent event) {
+
+    }
+
+}
