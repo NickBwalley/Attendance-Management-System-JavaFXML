db2928ff80d9103aad6434a2e9d05087f23ffc1e
NickBwalley <nickbwalley@gmail.com>
2021-08-24 13:20:14 +0300

    JavaFX-MiniProject

--- src/lims/Main.java
+++ src/lims/Main.java
@@ -1,4 +1,4 @@
-/*
+/*  
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,7 +19,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/lims/admin/AddBook.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/lims/admin/AddBookController.java
+++ src/lims/admin/AddBookController.java
@@ -1,9 +1,15 @@
 package lims.admin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import static lims.MainController.dbConnect;
 
 public class AddBookController {
 
@@ -16,8 +22,46 @@ public class AddBookController {
     @FXML
     private Button add_book;
 
+    //DATABASE CONNECTION
+    public static Connection dbConnect(){
+        
+        String url = "jdbc:mysql://localhost:3306/library_javafx";
+        String username = "root";
+        String password = "";
+        
+        //load drivers
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        
+        //database connection
+        try{
+            Connection conn = DriverManager.getConnection(url, username, password);
+            return conn;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
     @FXML
-    void addBook(ActionEvent event) {
+    void addBook(ActionEvent event) throws SQLException {
+        final String bookName = book_name.getText().toString();
+        final String bookAuthor = book_author.getText().toString();
+        
+        Connection conn = dbConnect();
+        Statement st = conn.createStatement();
+        String query = "INSERT INTO books(book_name, book_author) VALUES('"+bookName+"', '" +bookAuthor+"')";
+        st.executeUpdate(query);
+        conn.close();
+        
+          Alert a = new Alert(Alert.AlertType.INFORMATION);
+          a.setContentText(bookName + " successfully registered!");
+          a.show();
+        
 
     }
 
