78fee78931abfde65bc5d64918f5f579b31fd739
NickBwalley <nickbwalley@gmail.com>
2021-08-25 12:13:52 +0300

    PreLastCommit

--- src/lims/Book.java
+++ src/lims/Book.java
@@ -1,19 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lims;
 
-/**
- *
- * @author nickb
- */
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Book {
-    public String user_id;
-    public String book_id;
-    public String book_author;
-    public String book_status;
-    public String book_name;
+
+    private final  SimpleIntegerProperty id;
+    private final  SimpleStringProperty book_name;
+    private final  SimpleStringProperty book_author;
     
+    
+    public Book(int id, String book_name, String book_author) {
+        this.id = new SimpleIntegerProperty(id);
+        this.book_name = new SimpleStringProperty(book_name);
+        this.book_author = new SimpleStringProperty(book_author);
+        
+    }
+
+    public int getId() {
+        return id.get();
+    }
+    public void setId(int qnt) {
+        this.id.set(qnt);
+    }
+
+   
+
+    public String getBookName() {
+        return book_name.get();
+    }
+
+    public void setBookName(String book_name) {
+        this.book_name.set(book_name);
+    }
+    
+    public String getBookAuthor() {
+        return book_author.get();
+    }
+
+    public void setBookAuthor(String book_author) {
+        this.book_author.set(book_author);
+    }
 }
+    
--- src/lims/students/ViewBooksController.java
+++ src/lims/students/ViewBooksController.java
@@ -1,6 +1,8 @@
 package lims.students;
 
+import database.ConnectDB;
 import java.net.URL;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -19,19 +21,21 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import lims.Book;
 
-public class ViewBooksController /*implements Initializable*/ {
+public class ViewBooksController implements Initializable {
     @FXML
     private TableView<Book> table;
+    
     @FXML
-    private TableColumn<?, ?> book_id;
+    private TableColumn<Book, String > book_id;
 
     @FXML
-    private TableColumn<?, ?> book_name;
+    private TableColumn<Book, String> book_name;
 
     @FXML
-    private TableColumn<?, ?> book_author;
+    private TableColumn<Book, String> book_author;
     
     ObservableList<Book> obList= FXCollections.observableArrayList();
+    
     public static Connection ConnectDB(){
         String url = "jdbc:mysql://localhost:3306/library_javafx";
         String username = "root";
@@ -55,115 +59,43 @@ public class ViewBooksController /*implements Initializable*/ {
         }
         return null;
     }
-    public void fetchData() throws SQLException{
-//       try{
-//        Connection conn = ConnectDB();
-//        Statement st = conn.createStatement();  
-//        String query = "SELECT * FROM books";
-//        
-//        ResultSet rs = st.executeQuery(query);
-//        Book book = new Book();
-//        // System.out.println(rs.toString());
-//        if(rs.next()){
-//            book.book_id = rs.getString(1);
-//            book.book_name = rs.getString(2);
-//            book.book_author = rs.getString(3);
-//            // book.book_status = rs.getString(4);
-//            System.out.println(rs.getString(1) + rs.getString(2) + rs.getString(3));
-//            obList.add(book);
-//            conn.close();
-//        }
-//        
-//       } catch(SQLException e){
-//            e.printStackTrace();
-//       }
-    }
     
-//    @Override
-//    public void initialize(URL url, ResourceBundle rb){
-//        try {
-//            fetchData();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ViewBooksController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        book_id.setCellValueFactory(new PropertyValueFactory<>("id"));
-//        book_name.setCellValueFactory(new PropertyValueFactory<>("book_name"));
-//        book_author.setCellValueFactory(new PropertyValueFactory<>("book_author"));
-//        table.setItems(obList);
-//        
-//
-//    }
-    
-
-}
-/*private TableView<ModelTable> table;
-
-    @FXML
-    private TableColumn<ModelTable,String> ordernoCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> custCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> menuCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> deliveryCol;
-@Override
-    public void initialize(URL url, ResourceBundle rb) {
-     
-        ordernoCol.setCellValueFactory(new PropertyValueFactory<>("orderid"));
-        custCol.setCellValueFactory(new PropertyValueFactory<>("custid"));
-        menuCol.setCellValueFactory(new PropertyValueFactory<>("menuname"));
-         QuantityCol.setCellValueFactory(new PropertyValueFactory<>("qnt"));
-        deliveryCol.setCellValueFactory(new PropertyValueFactory<>("deliverytyp"));
-        addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
-       
-        tableConnection();
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        book_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        book_name.setCellValueFactory(new PropertyValueFactory<>("book_name"));
+        book_author.setCellValueFactory(new PropertyValueFactory<>("book_author"));
+        table.setItems(obList);
+        System.out.println(obList);
+        try {
+            tableConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(ViewBooksController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         table.setItems(obList);
         table.refresh();
         table.getSelectionModel().clearSelection();
-       
+
         
+
     }
-public void tableConnection(){
-        
+    
+    public void tableConnection()throws SQLException{
+    
         try {
-             
-            String query="{CALL `order_list`()}";
-            CallableStatement stmt = con.prepareCall(query);
-            
+            Connection conn = ConnectDB();
+            // String query="{CALL `books`()}";
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM books";
             ResultSet rs =stmt.executeQuery(query);
             while(rs.next()){
-                obList.add(new ModelTa
-*/
-
-/*private TableView<ModelTable> table;
-
-    @FXML
-    private TableColumn<ModelTable,String> ordernoCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> custCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> menuCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> deliveryCol;
-
-    @FXML
-    private TableColumn<ModelTable,String> addressCol;
-     @FXML
-    private TableColumn<ModelTable, String> QuantityCol;
-    
-    Connection con;
-    
-    ObservableList<ModelTable> obList= FXCollections.observableArrayList();
-    
-    public TakeOrderController(){
-        con=SqlConnection.Connector();
+                obList.add(new Book(rs.getInt(1),rs.getString(2),rs.getString(3)));
+                System.out.println(rs.getInt(1) + rs.getString(2) + rs.getString(3));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ViewBooksController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    @FXML
-*/
+    
+}
